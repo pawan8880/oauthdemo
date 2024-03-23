14ec70e4d26664c40c8c7f800db3cca8fdbad7ca
pawan8880 <pawan88java@gmail.com>
2024-03-23 21:15:37 +0530

    first commit

---
+++ main/java/com/oauth/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.oauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/home")
+	public String hello() {
+		return "Hello oAuth ho gaya";
+	}
+}
